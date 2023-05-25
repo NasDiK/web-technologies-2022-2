1ad080e6b6aa75e99796cdfa005fff15805cace9
a.s.tungusov <tungusov-aleksandr.fl@yandex.ru>
2023-05-25 23:34:21 +0500

    init commit, placed engine

--- index.php
+++ index.php
@@ -1,159 +1,74 @@
 <?php
-function task1($a, $b) {
-  if ($a >= 0 && $b >= 0) {
-    return $a - $b;
-  } elseif ($a < 0 && $b < 0) {
-    return $a * $b;
-  } elseif (($a < 0 && $b >= 0) || ($a >= 0 && $b < 0)) {
-    return $a + $b;
-  }
+define('TEMPLATES_DIR', 'templates/');
+define('LAYOUTS_DIR', 'layouts/');
+
+$page = 'index';
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
 }
 
-function task2($a) {
-  switch ($a) {
-    case 0:
-      echo '0 ';
-      $a++;
-    case 1:
-      echo '1 ';
-      $a++;
-    case 2:
-      echo '2 ';
-      $a++;
-    case 3:
-      echo '3 ';
-      $a++;
-    case 4:
-      echo '4 ';
-      $a++;
-    case 5:
-      echo '5 ';
-      $a++;
-    case 6:
-      echo '6 ';
-      $a++;
-    case 7:
-      echo '7 ';
-      $a++;
-    case 8:
-      echo '8 ';
-      $a++;
-    case 9:
-      echo '9 ';
-      $a++;
-    case 10:
-      echo '10 ';
-      $a++;
-    case 11:
-      echo '11 ';
-      $a++;
-    case 12:
-      echo '12 ';
-      $a++;
-    case 13:
-      echo '13 ';
-      $a++;
-    case 14:
-      echo '14 ';
-      $a++;
-    case 15:
-      echo '15';
-      $a++;
+$params = [];
+
+switch ($page) {
+    case 'index':
+        $params['title'] = 'Главная';
+        $params['test'] = 'Тестовая запись';
+        break;
+    case 'catalog':
+        $params['title'] = 'Каталог';
+        $params['catalog'] = getCatalog();
+        break;
+    case 'about':
+        $params['title'] = 'О нас';
+        $params['phone'] = '+7 495 123 23 23';
+        break;
+
+    case 'apicatalog':
+        echo json_encode(getCatalog(), JSON_UNESCAPED_UNICODE);
+        die();
+
     default:
-      return;
-  }
+        echo "404";
+        die();
 }
 
-//1
-$a = rand(0, 100);
-$b = rand(0, 100);
-
-echo task1($a, $b);
-
-//2
-echo '<hr/>';
-$a = rand(0, 15);
-
-task2($a);
-
-//3
-$a = 5;
-$b = 10;
-echo '<hr/>';
-function addition($a, $b) {
-    return $a + $b;
+function getCatalog() {
+    return [
+        [
+            'name' => 'Яблоко',
+            'price' => 24,
+            'image' => 'apple.png'
+        ],
+        [
+            'name' => 'Банан',
+            'price' => 1,
+            'image' => 'banana.png'
+        ],
+        [
+            'name' => 'Апельсин',
+            'price' => 12,
+            'image' => 'orange.png'
+        ],
+    ];
 }
 
-function subtraction($a, $b) {
-    return $a - $b;
+function renderTemplate($page, $params = []) {
+
+//    foreach ($params as $key => $value) {
+//            $$key = $value;
+//        }
+    extract($params);
+
+    ob_start();
+    include TEMPLATES_DIR . $page . ".php";
+    return ob_get_clean();
 }
 
-function multiplication($a, $b) {
-    return $a * $b;
-}
+//renderTemplate('index', $params);
 
-function division($a, $b) {
-  return $a / $b;
-}
-
-echo 'Сложение ' . addition($a, $b) . '<br/>';
-echo 'Вычитание ' . subtraction($a, $b) . '<br/>';
-echo 'Умножение ' . multiplication($a, $b) . '<br/>';
-echo 'Деление ' . division($a, $b) . '<br/>';
-
-//4
-echo '<hr/>';
-function mathOperation($arg1, $arg2, $operation)
-{
-  switch ($operation) {
-    case 'addition':
-      return addition($arg1, $arg2);
-    case 'subtraction':
-      return subtraction($arg1, $arg2);
-    case 'multiplication':
-      return multiplication($arg1, $arg2);
-    case 'division':
-      return division($arg1, $arg2);
-  }
-}
-
-echo mathOperation($a, $b, 'addition');
-//6
-echo '<hr/>';
-function power($val, $pow) {
-  if ($pow < 0) {
-    return 1;
-  }
-
-  if ($pow == 1) {
-      return $val;
-  }
- 
-  return  $val * power($val, $pow - 1);
-}
-
-echo power(2, 5);
-echo '<hr/>';
-
-//5 ниже!!!
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lesson17</title>
-</head>
-<body>
-<p><?php echo date('Y'); ?></p>
-<?php require('php_module.php') ?>
-<?php
-$year = date('Y');
-$content = file_get_contents('index.html');
-$content = str_replace('{{ year }}', $year, $content);
-echo $content; ?>
-</body>
-</html>
+echo renderTemplate(LAYOUTS_DIR . 'main', [
+    'title' => $params['title'],
+    'menu' => renderTemplate('menu'),
+    'content' => renderTemplate($page, $params)
+]);
