aea3349e33b0c168a246b37fd06c016742fe17d9
a.s.tungusov <tungusov-aleksandr.fl@yandex.ru>
2023-06-03 21:38:57 +0500

    lesson 15 done

--- src/pages/todos.js
+++ src/pages/todos.js
@@ -1,21 +1,85 @@
-import Auth from "../services/auth.js";
+import Todos from "../services/todos.js";
 import location from "../services/location.js";
 import loading from "../services/loading.js";
+import Form from "../components/form.js";
+import Auth from "../services/auth.js";
 
 const init = async () => {
-    const { ok: isLogged } = await Auth.me()
+    const {ok: isLogged, user:id} = await Auth.me()
+    const formEl = document.getElementById('todos')
+    const todos = document.getElementById('todos-items')
+    const {data} = await Auth.me()
 
     if (!isLogged) {
         return location.login()
     } else {
         loading.stop()
     }
+    const updateListTodos = () => {
+        loading.start();
+        const allTodos = Todos.getAll(data.user.id);
+        todos.innerHTML = ''
+        allTodos.then((x)=>{
+            todos.innerHTML = x.data.reduce((acc, {id, description}) => {
+                acc +=  `<div class="todo"><input id="c${id}" type="checkbox"> <div id="${id}">${description}</div></div>`;
 
-    // create POST /todo { description: string }
-    // get get /todo/1 - 1 это id
-    // getAll get /todo
-    // update put /todo/1 - 1 это id { description: string }
-    // delete delete /todo/1 - 1 это id
+                return acc;
+            }, '');;
+
+            x.data.forEach((el)=>{
+                const div = document.getElementById(el.id).addEventListener("click", () => deleteTODO(el.id), false);
+                const checkBox = document.getElementById(`c${el.id}`);
+                checkBox.checked = el.completed;
+                checkBox.onchange = (e) => updateStatusTodo(e, el.id);
+            })
+            loading.stop();
+        })
+    }
+
+    async function deleteTODO(id) {
+        const isDelete = confirm('Вы уверены?');
+        if (isDelete) {
+            loading.start();
+            const res = await Todos.deleteById(id);
+            if (res.ok) {
+                updateListTodos();
+            }
+        }
+    }
+
+    const addTodo = async (description) => {
+        loading.start();
+        const res = await Todos.add(description);
+        if(res.ok) {
+            updateListTodos();
+            const input = formEl.getElementsByTagName('input')[0];
+            input.value = '';
+        }
+    }
+
+    const updateStatusTodo = async (e, id) => {
+        const checkboxValue = e.target.checked;
+        e.target.checked = !e.target.checked;
+        loading.start();
+        const res = await Todos.updateById(id, checkboxValue);
+        if(res.ok) {
+            updateListTodos();
+        }
+    }
+
+    new Form(formEl, {
+        'description': (value) => {
+            if (value.length < 3) {
+                return 'Значение должно быть больше или равно 3'
+            } else if (value.length >= 32) {
+                return 'Значение должно быть меньше 32'
+            }
+
+            return false
+        }
+    }, (values) => addTodo(values));
+
+    updateListTodos();
 }
 
 if (document.readyState === 'loading') {
